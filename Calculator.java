ec83005a29ac7522ca1c9db4b6c05275588ce68a
prachijagota052 <prachijagota052@gmail.com>
2026-01-12 10:41:48 +0530

    Day 1

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator{
+    public static void main(String[] args) {
+        int a=10;
+        int b=15;
+        int result=a+b;
+        System.out.println("The sum is: " + result);
+        
+    }
+}
