83b4c10ac341e4c5fbe15746f281e1f911a8c2ca
prachijagota052 <prachijagota052@gmail.com>
2026-01-16 09:11:23 +0530

    homework

---
+++ Add.java
@@ -0,0 +1,11 @@
+import java.util.Scanner ;
+class Add{
+    public static void main (String[] args){
+        Scanner sc= new Scanner(System.in);
+        int a,b,sum;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        sum=a+b;
+        System.out.println("the sum is:"+sum);
+    }
+}
